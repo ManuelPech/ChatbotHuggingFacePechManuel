81206cc0ef050c3910eede8bc5a002d9e13313c9
ManuelPech <123490954+ManuelPech@users.noreply.github.com>
2024-05-24 22:45:40 -0600

    Update huggingChat.py

--- huggingChat.py
+++ huggingChat.py
@@ -3,6 +3,7 @@ from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain import PromptTemplate, HuggingFaceHub, LLMChain
+from langchain_community.llms import HuggingFaceHub
 from dotenv import load_dotenv
 
 # load the Environment Variables. 
